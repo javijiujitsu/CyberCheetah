c7830c03f8eda4b4b432d376d80dd2d800aec384
Javier Buitrago <jbuitrago2014@fau.edu>
2017-12-16 01:28:25 -0500

    defined career model

---
+++ models/career.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const careerSchema = new Schema(
+	{
+		name:{
+			type: String,
+			required: [true, "name required."]
+		},
+		description:{
+			type: String,
+		},
+		universities:{
+			type: String,
+		},
+    certification:{
+			type: String,
+		},
+    resource:{
+			type: String,
+		},
+    idtask:[{
+      type: Schema.Types.ObjectId,
+			id: String,
+		}],
+
+	},
+	{
+    timestamps: { createdAt: "created_at", updatedAt: "updated_at" }
+}
+);
+
+const CareerModel = mongoose.model("Career", careerSchema);
+
+module.exports = CareerModel;

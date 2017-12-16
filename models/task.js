f456571202ac5ae9b1ace906ddecc8d36d19d749
Javier Buitrago <jbuitrago2014@fau.edu>
2017-12-16 01:39:39 -0500

    added task model

---
+++ models/task.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const taskSchema = new Schema(
+	{
+		name:{
+			type: String,
+			required: [true, "name required."]
+		},
+		description:{
+			type: String,
+		},
+		estimatedlength:{
+			type: Number,
+		},
+    resource:{
+			type: String,
+		},
+
+	},
+	{
+    timestamps: { createdAt: "created_at", updatedAt: "updated_at" }
+}
+);
+
+const TaskModel = mongoose.model("Task", taskSchema);
+
+module.exports = TaskModel;

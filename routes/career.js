f380668320831ba76f20ab79ac1824733f4cfadf
Javier Buitrago <jbuitrago2014@fau.edu>
2017-12-16 16:48:09 -0500

    added post for careers

--- routes/career.js
+++ routes/career.js
@@ -14,4 +14,30 @@ router.get('/getcareer', (req, res, next) => {
   });
 });
 
+/* CREATE a new Career. */
+router.post('/createcareer', (req, res, next) => {
+  const newCareer = new Career({
+    name: req.body.name,
+    description: req.body.description,
+    universities: req.body.universities,
+    certification: req.body.certification,
+    resource: req.body.resource,
+    idtask: req.body.idtask
+    //ID task is being converting to a string
+
+  });
+
+  newCareer.save((err) => {
+    if (err) {
+      res.json(err);
+      return;
+    }
+
+    res.json({
+      message: 'New Career created!',
+      id: newCareer._id
+    });
+  });
+});
+
 module.exports = router;

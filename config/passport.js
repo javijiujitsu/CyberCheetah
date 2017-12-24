6befe01cd0ba5bc9aa0b78f296fa5bd95062723e
Javier Buitrago <jbuitrago2014@fau.edu>
2017-12-24 14:51:59 -0500

    updated the local strategy

--- config/passport.js
+++ config/passport.js
@@ -4,50 +4,57 @@ const bcrypt        = require('bcrypt');
 
 const User          = require('../models/user');
 
-passport.serializeUser((userFromDb, done) => {
-	done(null, userFromDb._id);
+// Save the user's ID in the bowl (called when user logs in)
+passport.serializeUser((userFromDb, next) => {
+    next(null, userFromDb._id);
 });
 
-passport.deserializeUser((idFromSession, done) => {
-	User.findById(
-		idFromSession,
-		(err, userFromDb) => {
-			if(err){
-				done(err);
-				return;
-			}
-			done(null, userFromDb);
-		}
-	);
+
+// Retrieve the user's info from the DB with the ID we got from the bowl
+passport.deserializeUser((idFromBowl, next) => {
+    User.findById(
+      idFromBowl,
+      (err, userFromDb) => {
+          if (err) {
+            next(err);
+            return;
+          }
+
+          next(null, userFromDb);
+      }
+    );
 });
 
-passport.use(
-	new LocalStrategy(
-		{
-			usernameField: 'loginUsername',
-			passwordField: 'loginPassword'
-		},
-		(sentUsername, sentPassword, done) => {
-			User.findOne(
-				{ username: sentUsername },
-				(err, userFromDb) => {
-					if(err){
-						done(err);
-						return;
-					}
-					if(!userFromDb){
-						done(null, false, { message: "Wrong Username" });
-						return;
-					}
-					const isPasswordGood =
-					bcrypt.compareSync(sentPassword, userFromDb.password);
-					if(!isPasswordGood){
-						done(null, false, { message: "Wrong Password" });
-						return;
-					}
-					done(null, userFromDb);
-				}
-			);
-		}
-	)
-);
+
+// email & password login strategy
+passport.use(new LocalStrategy(
+  {
+    username: 'blahEmail',    // sent through AJAX from Angular
+    password: 'blahPassword'  // sent through AJAX from Angular
+  },
+  (theEmail, thePassword, next) => {
+
+      User.findOne(
+        { email: theEmail },
+        (err, userFromDb) => {
+            if (err) {
+              next(err);
+              return;
+            }
+
+            if (userFromDb === null) {
+              next(null, false, { message: 'Incorrect email' });
+              return;
+            }
+
+            if (bcrypt.compareSync(thePassword, userFromDb.encryptedPassword) === false) {
+              next(null, false, { message: 'Incorrect password' });
+              return;
+            }
+
+            next(null, userFromDb);
+        }
+      ); // close User.findOne()
+
+  } // close (theEmail, thePassword, next) => {
+));

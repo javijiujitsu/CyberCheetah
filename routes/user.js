d5f280dc6347e6f909fd2b18c6ba73d82a78b697
Javier Buitrago <jbuitrago2014@fau.edu>
2017-12-23 20:58:31 -0500

    Get for user profile

--- routes/user.js
+++ routes/user.js
@@ -6,7 +6,25 @@ const Career  = require('../models/career');
 const User = require('../models/user');
 const { ensureLoggedIn, ensureLoggedOut } = require('connect-ensure-login');
 
+// Returning the profile for the User
+router.get('/userprofile', (req, res, next) => {
+  User.find((err, profileList) => {
+    if (err) {
+      res.json(err);
+      return;
+    }
+    res.json(profileList);
+  });
+});
 
+
+
+
+
+
+
+
+module.exports = router;
 
 
 

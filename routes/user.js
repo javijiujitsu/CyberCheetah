23cbd40227728f0a80bafc321eb82d2ed53006f2
Javier Buitrago <jbuitrago2014@fau.edu>
2017-12-23 19:06:48 -0500

    added user model

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ var router    = express.Router();
 var mongoose  = require('mongoose');
 
 const Career  = require('../models/career');
+const User = require('../models/user');
 const { ensureLoggedIn, ensureLoggedOut } = require('connect-ensure-login');
 
 

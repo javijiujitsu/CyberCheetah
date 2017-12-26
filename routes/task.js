e31f0579badd69cf0d4b68ca4e86f4de43bdaf8f
Javier Buitrago <jbuitrago2014@fau.edu>
2017-12-25 21:26:28 -0500

    post for saved task added

--- routes/task.js
+++ routes/task.js
@@ -6,24 +6,28 @@ const User    = require('../models/user');
 const Career  = require('../models/career');
 const Task    = require('../models/task');
 
+/* Save a new task. */
+router.post('/savecreatedtask', (req, res, next) => {
+  const saveTask = new Task({
+    name: req.body.name,
+    description: req.body.description,
+    estimatedlength: req.body.estimatedlength,
+    created_: req.body.created_,
+    resource: req.body.resource
+  });
 
+  saveTask.save((err) => {
+    if (err) {
+      res.json(err);
+      return;
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    res.json({
+      message: 'New Task Saved!',
+      id: saveTask._id
+    });
+  });
+});
 
 
 
